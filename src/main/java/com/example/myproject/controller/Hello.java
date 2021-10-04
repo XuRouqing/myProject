756f98a3b3b35b6926fa12ae7f019fd5dbe536b9
XuRouqing <9577036+xu-rouqing@user.noreply.gitee.com>
2021-10-04 18:16:44 +0800

    Initial commit

---
+++ src/main/java/com/example/myproject/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.example.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
